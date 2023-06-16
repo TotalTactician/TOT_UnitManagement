6012763d14c8d3288ce514fd502f23bdb5f2ba84
AsterosTheGreat <sylvestersnijders007@gmail.com>
2023-06-16 09:34:12 +0200

    Tests

--- test_main.py
+++ test_main.py
@@ -10,17 +10,12 @@ def app():
     app.config['TESTING'] = True
     yield app
 
-def test_getUnit(app):
-    # Create a mock database and collection
-    mock_db = {"units": {}}
-    app.db = mock_db
+def test_getUnit(app, mocker):
 
     # Define a sample unit
-    sample_unit = {"_id": ObjectId(), "name": "Test Unit"}
+    sample_unit = {"_id": ObjectId("647f10601bf12007cc8b55c6"), "name": "Test Unit"}
+    mocker.patch.object(app.db, "find_one", return_value=sample_unit)
 
-    # Add the sample unit to the mock collection
-    mock_db["units"][str(sample_unit["_id"])] = sample_unit
-    print(mock_db)
     # Make a request to the API endpoint
     response = app.test_client().get(f"/api/unit/{sample_unit['_id']}")
 
@@ -30,10 +25,5 @@ def test_getUnit(app):
     # Check that the response data matches the sample unit
     print(response.data)
     assert json.loads(response.data) == json.loads(json.dumps(sample_unit, default=str))
-
-    # Check that the find_one method was called with the correct argument
-    assert "units" in mock_db
-    assert str(sample_unit["_id"]) in mock_db["units"]
-    assert mock_db["units"][str(sample_unit["_id"])] == sample_unit
 
 
